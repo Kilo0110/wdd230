e31fe775192b698751b54d8d09aaa955b98c711d
KamCy Ahaka <kamcyahaka@gmail.com>
2024-01-18 22:37:23 +0100

    Completed page enhancements

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,5 +1,9 @@
 const themeModeToggle = document.querySelector('.theme-mode-toggle');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
 
 themeModeToggle.addEventListener('click', () => {
   themeModeToggle.classList.toggle('dark');
+  body.classList.toggle('dark');
+  main.classList.toggle('dark');
 });
