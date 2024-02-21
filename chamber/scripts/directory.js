2298ea1e901bb2baea166eaac3e21fc1d0f9f858
KamCy Ahaka <kamcyahaka@gmail.com>
2024-02-21 02:32:18 +0100

    Code enhancements

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,13 @@
+const companyCardContainer = document.querySelector('.company-card-container')
+
+function createCardLayoutElements() {
+  const companyCardDiv = document.createElement('div')
+  const companyLogo = document.createElement('img')
+  const companyName = document.createElement('h2')
+  const companyAbout = document.createElement('p')
+  const companyAddress = document.createElement('p')
+  const cardFooter = document.createElement('div')
+  const cardTable = document.createElement('table')
+  const cardTableHead = document.createElement('thead')
+  const cardTableBody = document.createElement('tbody')
+}
